1ef06e18ee0b602206ebacbda9b3eb7b0e916f8c
Nathan Malitz <nmalitz@icloud.com>
2023-05-27 15:45:02 -0400

    first commit

---
+++ soundswap/soundswap/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+from core.views import frontpage, about
+
+urlpatterns = [
+    path("", frontpage, name="frontpage"),
+    path("about/", about, name="about"),
+    path("admin/", admin.site.urls),
+]
